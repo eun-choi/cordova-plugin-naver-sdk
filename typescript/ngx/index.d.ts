8cc1d5ec9867302cb377120c233be4a0f4f2305c
eun-choi <54615424+eun-choi@users.noreply.github.com>
2023-04-19 15:03:05 +0900

    delete typescript

--- typescript/ngx/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { IonicNativePlugin } from '@ionic-native/core';
-export declare class NaverCordovaSDK extends IonicNativePlugin {
-    login(): Promise<any>;
-    logout(): Promise<any>;
-    unlinkApp(): Promise<any>;
-    refreshAccessToken(): Promise<string>;
-    getAccessToken(): Promise<string>;
-}
